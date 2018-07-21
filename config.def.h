8f71e65dd98344276803bbee2bf1f1d27b84b3a8
Lukáš Hozda <luk.hozda@gmail.com>
2018-07-21 18:50:26 +0200

    improve configuration

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ProFontWindowsCzech:size=9", "monospace:size=10" };
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "audio", "firefox", "editor", "term", "extra" };
+static const char *tags[] = { "audio", "firefox", "editor", "term", "offload", "extra" };
 
 static const Rule rules[] = {
 	/* xprop(1):
