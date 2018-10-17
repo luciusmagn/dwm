cea8f5d951c68df0bae62f37954fa91202238d61
Lukáš Hozda <luk.hozda@gmail.com>
2018-10-17 16:32:34 +0200

    fix font name in config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "ProFontWindowsCzech:size=9", "monospace:size=10" };
-static const char dmenufont[]       = "ProFontWindowsCzech:size=9";
+static const char *fonts[]          = { "ProFontCzech:size=9", "monospace:size=10" };
+static const char dmenufont[]       = "ProFontCzech:size=9";
 static const char col_gray1[]       = "#191716";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
